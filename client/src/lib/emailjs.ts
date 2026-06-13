74ca3aa8397c264dcc4646aacd2d62bf9bcf2bed
Mohamed Sabeek <safeesafee0000@gmail.com>
2026-06-13 15:07:43 +0530

    fixed template

--- client/src/lib/emailjs.ts
+++ client/src/lib/emailjs.ts
@@ -19,8 +19,6 @@ export const sendEmail = async (formData: { name: string, email: string, message
     message: formData.message,
   };
 
-  console.log("EmailJS Payload:", templateParams);
-
   const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
   const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
   const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
